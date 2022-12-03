f2221c1255b9d2c8d62a286162e3a53b5cc80dcb
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-12-03 23:20:43 +0100

    Add queries for lab 3, instructors and ensemble

--- database/lab 3/queries.sql
+++ database/lab 3/queries.sql
@@ -29,3 +29,50 @@ UNION SELECT COUNT(*), 0
 FROM student WHERE student_id
 NOT IN (SELECT student_id from sibling)
 ORDER BY siblings;
+
+--Get total lessons per instructor
+SELECT p.personal_number, p.first_name, p.last_name, COUNT(timeslot_id) AS lessons
+FROM timeslot AS ts
+NATURAL JOIN instructor AS i
+INNER JOIN person AS p ON i.instructor_id=p.person_id
+GROUP BY p.personal_number, p.first_name, p.last_name
+ORDER BY COUNT(timeslot_id) DESC;
+
+--Get amount of instructors lessons for the current month
+SELECT p.personal_number, p.first_name, p.last_name, COUNT(timeslot_id) AS lessons
+FROM timeslot AS ts
+NATURAL JOIN instructor AS i
+INNER JOIN person AS p ON i.instructor_id=p.person_id
+WHERE (SELECT EXTRACT(MONTH FROM ts.date) = EXTRACT(MONTH FROM CURRENT_DATE))
+GROUP BY p.personal_number, p.first_name, p.last_name
+ORDER BY COUNT(timeslot_id) DESC;
+
+--Get all ensembles with genre
+SELECT date, genre
+FROM timeslot
+NATURAL JOIN ensemble
+NATURAL JOIN genre
+ORDER BY date;
+
+--Get all ensembles next week
+SELECT ts.date,
+CASE WHEN EXTRACT(DOW FROM ts.date) = 0 THEN 'Monday'
+	WHEN EXTRACT(DOW FROM ts.date) = 1 THEN 'Tuesday'
+	WHEN EXTRACT(DOW FROM ts.date) = 2 THEN 'Wednesday'
+	WHEN EXTRACT(DOW FROM ts.date) = 3 THEN 'Thursday'
+	WHEN EXTRACT(DOW FROM ts.date) = 4 THEN 'Friday'
+	WHEN EXTRACT(DOW FROM ts.date) = 5 THEN 'Saturday'
+	WHEN EXTRACT(DOW FROM ts.date) = 6 THEN 'Sunday'
+END AS day, g.genre, 
+CASE WHEN e.max_students - COUNT(student_id) FILTER (WHERE student_id IN (SELECT student_id FROM ensemble_student)) > 2 THEN '2+ spots left'
+	WHEN e.max_students - COUNT(student_id) FILTER (WHERE student_id IN (SELECT student_id FROM ensemble_student)) = 2 THEN '2 spots left'
+	WHEN e.max_students - COUNT(student_id) FILTER (WHERE student_id IN (SELECT student_id FROM ensemble_student)) = 1 THEN '1 spot left'
+	WHEN e.max_students - COUNT(student_id) FILTER (WHERE student_id IN (SELECT student_id FROM ensemble_student)) = 0 THEN 'Fully booked'
+	WHEN e.max_students - COUNT(student_id) FILTER (WHERE student_id IN (SELECT student_id FROM ensemble_student)) < 0 THEN 'Over booked'
+END AS spots
+FROM timeslot AS ts
+NATURAL JOIN ensemble AS e
+NATURAL JOIN genre AS g
+NATURAL JOIN ensemble_student AS es
+WHERE (SELECT EXTRACT(WEEK FROM ts.date) = EXTRACT(WEEK FROM CURRENT_DATE + INTERVAL '1 WEEK'))
+GROUP BY ts.date, g.genre, e.max_students ORDER BY EXTRACT(DOW FROM ts.date), genre;
